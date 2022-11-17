e1d7f2d72a051914cd950fe698bf14bfbbf166ee
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-18 00:44:46 +0600

    appointment option api called

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 // middle ware
 
 app.use(cors());
@@ -11,18 +11,30 @@ app.use(express.json());
 
 // mongodb connection list
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.vycotd7.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vycotd7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+// crud operation running here
+
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("doctorPortal")
+      .collection("ApointmentOption");
+
+    app.get("/appointmentsOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 
 //mongodb connection list end
 
