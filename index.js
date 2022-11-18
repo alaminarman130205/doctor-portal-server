e949a03d7fa9588923325893ac0dc0419b42d0d4
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-18 10:29:14 +0600

    single data match and set done for  slot

--- index.js
+++ index.js
@@ -25,11 +25,49 @@ async function run() {
     const appointmentOptionCollection = client
       .db("doctorPortal")
       .collection("ApointmentOption");
+    const bookingCollection = client.db("doctorPortal").collection("bookings");
+
+    //fetching data from mongobd
 
     app.get("/appointmentsOptions", async (req, res) => {
+      const date = req.query.date;
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
+
+      // get the bookings of the provided date
+      const bookingQuery = { appointmentDate: date };
+      const alreadyBooked = await bookingCollection
+        .find(bookingQuery)
+        .toArray();
+
+      // code carefully :D
+      options.forEach((option) => {
+        const optionBooked = alreadyBooked.filter(
+          (book) => book.treatment === option.name
+        );
+        const bookedSlots = optionBooked.map((book) => book.slot);
+        const remainingSlots = option.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        console.log(date, option.name, bookedSlots, remainingSlots.length);
+        option.slots = remainingSlots;
+      });
       res.send(options);
+    });
+    /*
+    *api naming convention
+    app.get('/bookings') all bookings info fetch by this name convention
+    app.get('/bookings/:id') single id fetch by this name convention
+    app.post('/bookings') single bookings info post 
+    app.patch('/bookings/:id') update one info by this convention
+    app.delete('/bookings:id/:id')delete one info from this convention
+
+    */
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     });
   } finally {
   }
