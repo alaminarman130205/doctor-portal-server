3308f3e51e7dc5c0985330589bf9e7fd03afc8d9
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-21 21:34:59 +0600

    checkout card creted

--- index.js
+++ index.js
@@ -134,6 +134,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    });
+
     app.get("/jwt", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
