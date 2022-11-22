dddd2e2c31965ed3644ae8e243aa004a70b4c580
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-11-22 19:02:53 +0600

    final project done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 // middle ware
 
 app.use(cors());
@@ -44,6 +45,7 @@ async function run() {
     const bookingCollection = client.db("doctorPortal").collection("bookings");
     const usersCollection = client.db("doctorPortal").collection("users");
     const doctorCollection = client.db("doctorPortal").collection("doctors");
+    const paymentCollection = client.db("doctorPortal").collection("payments");
 
     const varifyAdmin = async (req, res, next) => {
       const decodeEmail = req.decoded.email;
@@ -139,6 +141,38 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const booking = await bookingCollection.findOne(query);
       res.send(booking);
+    });
+
+    //payment api
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+      const _id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updateResult = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     app.get("/jwt", async (req, res) => {
